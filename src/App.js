a56bc6dc0db2b92ea7239bda867b2c94a692cc98
finlay888 <finlayjmm@gmail.com>
2020-12-28 17:33:22 +0000

    styling added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Scroll from './Scroll';
+import './App.css'
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({robots: users}))
 	}
 
 	onSearchChange = (event) => {
 		this.setState({ searchfield: event.target.value})
 	}
 
-	render () {
+	render() {
 		const filteredRobots = this.state.robots.filter(robot =>{
 			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 		})
 		return (
 			<div className='tc'>
-				<h1>Robofriends</h1>
+				<h1 className='f1 light-green'>Robofriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={filteredRobots}/>
+				<Scroll>
+					<CardList robots={filteredRobots}/>
+				</Scroll>
+				
 			</div>
-		)
+		);
 	}
 }
 
